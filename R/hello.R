66f0f174b710d090188e452521e640c8d7c0a432
Avishai Wizel <Avishai987@gmail.com>
2023-03-05 11:57:30 +0200

    add unnumbered

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,5 @@
 print_tab <- function(plt,title,subtitle_num = 3) {
-  cat(rep("#",subtitle_num) %>% paste(collapse = ''), " " , title," \n") # e.g "### UMAP"
+  cat(rep("#",subtitle_num) %>% paste(collapse = ''), " " , title,"{.unnumbered }", " \n") # e.g "### UMAP"
   print(plt)
   plot.new()
   dev.off()

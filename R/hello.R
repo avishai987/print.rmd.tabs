ff5e0a8b46bf520e406c0b4b78780afec1f14eae
Avishai Wizel <Avishai987@gmail.com>
2023-03-05 12:15:43 +0200

    default subtitle

--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,4 @@
-print_tab <- function(plt,title,subtitle_num = 3) {
+print_tab <- function(plt,title,subtitle_num = 2) {
   cat(rep("#",subtitle_num) %>% paste(collapse = ''), " " , title,"{.unnumbered }", " \n") # e.g "### UMAP"
   print(plt)
   plot.new()
